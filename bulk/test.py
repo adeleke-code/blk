7a533c28891af9a8c83ec0ee59744569d2303b68
adeleke-code <femiadeleke601@gmail.com>
2023-11-30 15:31:34 +0100

    proxy test

--- bulk/test.py
+++ bulk/test.py
@@ -7,6 +7,7 @@ from dotenv import find_dotenv, load_dotenv
 import os, time
 
 load_dotenv(find_dotenv())
+from .helpers import *
 
 
 account_sid = os.getenv('ssd_id')
@@ -19,22 +20,39 @@ client = Client(account_sid, auth_token)
 
 
 
-def test(number):
+def test():
+    while True:
+        print("""To exit, type 'exit',
+                To test input correct phone number and press enter.
+            """)
 
-    try:
-        res = client.messages.create(
-        from_='Test Blk',
-        body="Hi, This is a proxy test message.",
-        to=number
-            )
-        time.sleep(2)
-        print(f"Message sent successfully to {number}")
-        MessagesSent.objects.create(phone=number, status='sent')
+        res = input(":>>>>> ")
+        if res == "":
+            print("Please enter a valid command.")
+            
+        elif res == "exit":
+            print("Exiting...")
+            time.sleep(2)
+            clear()
+            sys.exit()
 
-        
-    except Exception as e:
-        logging.error("An error occurred: %s", str(e))
-        print(f"Message not sent to {number}")
-        MessagesSent.objects.create(phone=number, status='failed')
+        if res != "" and res != "exit":
+           
+            try:
+                res = client.messages.create(
+                from_='Test Blk',
+                body="Hi, This is a proxy test message.",
+                to=res
+                    )
+                time.sleep(2)
+                print(f"Message sent successfully to {res}")
+                MessagesSent.objects.create(phone=res, status='sent')
 
-        return
+                
+            except Exception as e:
+                logging.error("An error occurred: %s", str(e))
+                print(f"Message not sent to {res}")
+                print("Enter a valid phone number.")
+                MessagesSent.objects.create(phone=res, status='failed')
+
+                return
